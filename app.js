2f50b0463a67ae89216832c6700f84a63ffbda03
luxy29 <karvey.john@gmail.com>
2017-09-17 07:28:22 +0300

    Added all files.

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+let app = express();
+
+// Middleware
+
+// Views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body parsr middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/bots', (req, res) => {
+    res.render('bots');
+});
+
+app.listen(8000, () => {
+    console.log('Server started on port 8888');
+});
